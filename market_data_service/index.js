cc3bd26ec47df480b01e282ebeac8f62058f3b1f
AakashJha04 <aakashthedeveloper04@gmail.com>
2025-04-03 13:20:48 +0530

    integrated Client and market_data_service microservices

--- market_data_service/index.js
+++ market_data_service/index.js
@@ -3,8 +3,10 @@
 import express from 'express';
 import dotenv from "dotenv"
 import UpstoxClient from "upstox-js-sdk";
+import cors from "cors";
 dotenv.config();
 const app = express();
+app.use(cors());
 const port = 4000;
 
 const authurlfortoken_sample = `https://api.upstox.com/v2/login/authorization/dialog?response_type=code&client_id=600c18c0-401c-4387-96e2-ee6d15cc83bb&redirect_uri=https%3A%2F%2Flocalhost%3A4000`;
@@ -80,6 +82,62 @@ const getMarketQuoteOHLC = (symbol, callback) => {
        }
    );
 };
+
+// get historical data
+// app.get("/getHistoricalData", (req, res) => {
+//     console.log("Getting Historical route");
+//     const symbol = req.query.symbol;
+//     getHistoricalData(symbol, (err, data) => {
+//         if (err) {
+//             res.status(500).json("failed")
+//         } else {
+//             res.status(200).json(data)
+//         }
+//     });
+//  });
+
+//  const getHistoricalData = (symbol, callback) => {
+//     let apiInstance = new UpstoxClient.HistoryApi();
+//     let apiVersion = "2.0"; 
+//     let interval = "1minute"; 
+//     let toDate = "2024-05-12";
+//     let fromDate = "2024-05-10";
+
+//     apiInstance.getHistoricalCandleData1(symbol, interval, toDate, fromDate,apiVersion, (error, data, response) => {
+//         if (error) {
+//         console.error(error);
+//         callback(err, null)
+//         } else {
+//         console.log('API called successfully. Returned data: ' + JSON.stringify(data));
+//         callback(null, data)
+//         }
+//     });
+//  }
+
+// get monthly candles
+app.get("/getDataMonthlyInterval", (req, res) => {
+    const instrumentKey = req.query.instrumentKey;
+    let apiVersion = "2.0";
+    let interval = "month";
+    let toDate = "2024-05-12";
+    let fromDate = "2023-05-12";
+
+    let defaultClient = UpstoxClient.ApiClient.instance;
+    var OAUTH2 = defaultClient.authentications["OAUTH2"];
+    OAUTH2.accessToken = process.env.ACCESS_TOKEN;
+    let apiInstance = new UpstoxClient.HistoryApi();
+    apiInstance.getHistoricalCandleData1(instrumentKey, interval, toDate, fromDate, apiVersion, (error, data, response) => {
+        if (error) {
+            res.status(500).json({"error": error})
+        } else {
+            res.status(200).json({"data": data.data})
+            console.log(
+                "API called successfully. Returned data: " + JSON.stringify(data)
+            );
+        }
+    });
+})
+
 
 app.listen(port, () => {
    console.log(`Server is listening at http://localhost:${port}`);
